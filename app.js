e93c379e8338002a745561657356feb68d2aeb0e
hoodielive <hoodielive@gmail.com>
2018-09-04 21:49:06 -0400

    typescripting

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
 "use strict";
-console.log("It works!");
+// string
+var myName = 'Larry';
+// number
+var myAge = 19;
+// boolean
+var hasHobbies = false;
+// assigning a type 
+var myRealAge;
+myRealAge: 33;
+// array
+var hobbies = ["coding", "reading"];
+console.log(typeof hobbies);
+hobbies = [100];
+// tuples 
+var address = ["Superstreet", 99];
+// enum
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+    Color[Color["Pink"] = 100] = "Pink";
+})(Color || (Color = {}));
+var myColor = Color.Green;
+console.log(myColor);
--- app.ts
+++ app.ts
@@ -1,1 +1,92 @@
-console.log("It works!")
+// string
+let myName: string = 'Larry'
+
+// number
+let myAge: number = 19
+
+// boolean
+let hasHobbies: Boolean = false; 
+
+// assigning a type 
+let myRealAge: number; 
+myRealAge: 33
+
+// array
+let hobbies: any[] = ["coding", "reading"]; 
+console.log(typeof hobbies)
+hobbies = [100]; 
+
+// tuples 
+let address: [string, number] = ["Superstreet", 99]
+
+// enum
+enum Color {
+    Gray,
+    Green,
+    Blue,
+    Pink = 100, // lol 
+}
+
+let myColor: Color = Color.Green;
+console.log(myColor);
+
+// any 
+let car: any = "BMW";
+console.log(car); 
+car = { brand: "BMW", series: 3};
+console.log(car); 
+
+// functions 
+function returnMyName(): string {
+  return myName;
+}
+console.log(returnMyName());
+
+// void 
+function sayHello(): void {
+  console.log("Hello!");
+  // this function should NOT return anything, if you try to coerce it, typescript will fuss at you
+}
+
+// argument types 
+function multiply(value1: number, value2: number): number { 
+  return value1 * value2;
+}
+console.log(multiply(2, 218))
+
+// function types 
+let myMultiply: (val1: number, val2: number) => number; 
+// myMultiply = sayHello;
+// myMultiply(); 
+myMultiply = multiply
+console.log(myMultiply(5, 2));
+
+// objects 
+//let userData: { name: string, age: number } = 
+//  name: "Hood",
+//  age: 33,
+//};
+
+type Complex = { data: number[], output: (all: boolean) => number[]}; 
+
+// Complex object 
+let complex: Complex = { 
+
+    data: [100, 3.99, 10],
+
+    output: function (all: boolean): number[] {
+      return this.data;  
+    }
+}
+
+
+// union types
+myRealRealAge: number | string = 29; 
+myRealRealAge = 23
+console.log(myRealRealAge); 
+
+// Check types 
+let finalValue = "A String",
+  if (typeof finalvalue == "number") {
+    console.log("Final vaue is a number") 
+  }
