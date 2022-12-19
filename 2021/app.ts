4395ba1c31ff76af433a527bb019cc974056ed50
hoodielive <hoodielive@gmail.com>
2022-12-18 22:02:57 -0500

    Typescript a superset of the javascript phuckery - thank you

--- 2021/app.ts
+++ 2021/app.ts
@@ -9,7 +9,7 @@ function sendEmail(value: IEmailable): void
   console.log(value);
 }
 
-sendEmail({name: "Toluwalase", email: "something@email.com"});
+sendEmail({name: "enilo", email: "something@email.com"});
 
 class Company implements IEmailable
 {
@@ -23,9 +23,8 @@ class Company implements IEmailable
   }
 }
 
-let treehouse = new Company("TreeHouse", "tolu@tolu.com");
+let treehouse = new Company("EgunHouse", "enilo@enilo.com");
 sendEmail(treehouse);
-
 
 let CompanyTwo = (function () {
   function CompanyTwo(companyName: string, emailAddress: string) 
@@ -35,3 +34,58 @@ let CompanyTwo = (function () {
   }
   return CompanyTwo;
 })();
+
+class Song
+{
+  title: string;
+  constructor(
+    title: string,
+    public artist: string,
+    public duration: string,
+    public isPlaying: boolean = false
+  ) 
+    {
+      this.title = title;
+    }
+
+  play() {
+    this.isPlaying = true;
+  };
+
+  stop() {
+    this.isPlaying = false;
+  };
+
+  toHTML() {
+    let currentClass = this.isPlaying ? "current" : "";
+
+    return `<li class="${currentClass}">
+      ${this.title} - ${this.artist}
+      <span class="duration">${this.duration}</span>
+    </li>; `
+  }
+}
+
+
+class Playlist 
+{
+  songs: Song[];
+  nowPlayingIndex: number;
+  currentSong: string;
+
+  constructor() {
+    this.songs = [];
+    this.nowPlayingIndex = 0;
+    this.currentSong = "";
+  }
+
+  add(song: Song)
+  {
+    this.songs.push(song);
+  }
+
+  play()
+  {
+    this.currentSong;
+  }
+}
