83a62fe9178faf4e514391739ff0e694597b1346
hoodielive <hoodielive@gmail.com>
2022-12-18 21:52:11 -0500

    Typescript

--- 2021/app.ts
+++ 2021/app.ts
@@ -11,7 +11,27 @@ function sendEmail(value: IEmailable): void
 
 sendEmail({name: "Toluwalase", email: "something@email.com"});
 
-class Company
+class Company implements IEmailable
 {
-  constructor(self,)
+  name: string;
+  email: string;
+  
+  constructor(companyName: string, emailAddress: string)
+  {
+    this.name = companyName;
+    this.email = emailAddress;
+  }
 }
+
+let treehouse = new Company("TreeHouse", "tolu@tolu.com");
+sendEmail(treehouse);
+
+
+let CompanyTwo = (function () {
+  function CompanyTwo(companyName: string, emailAddress: string) 
+  {
+    this.name = companyName;
+    this.email =  emailAddress;
+  }
+  return CompanyTwo;
+})();
