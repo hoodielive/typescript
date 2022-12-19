ab042b3eef29e0e8b5ebab84f383884837834bb1
hoodielive <hoodielive@gmail.com>
2022-12-18 21:42:05 -0500

    Typescriptus

---
+++ 2021/app.js
@@ -0,0 +1,5 @@
+"use strict";
+function calculateTax(value) {
+    console.log(value);
+}
+calculateTax("Hello");
--- 2021/app.ts
+++ 2021/app.ts
@@ -1,1 +1,17 @@
-function calculateTax
+interface IEmailable 
+{
+  name: string,
+  email: string,
+}
+
+function sendEmail(value: IEmailable): void
+{
+  console.log(value);
+}
+
+sendEmail({name: "Toluwalase", email: "something@email.com"});
+
+class Company
+{
+  constructor(self,)
+}
