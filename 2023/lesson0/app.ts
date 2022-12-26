9fe1a7d3f64e2f428e0c9b16b267537a6171f8d8
hoodielive <hoodielive@gmail.com>
2022-12-26 01:37:39 -0500

    Typescript lesson 0

--- 2023/lesson0/app.ts
+++ 2023/lesson0/app.ts
@@ -1,3 +1,5 @@
+const log = console.log
+
 const greeting: string = 'Hello, World!';
 
 const year: number = 2021;
@@ -9,11 +11,18 @@ greeting.toLowerCase();
 const addTwoNumbers = (a: number, b: number): number => a + b;
 
 
-interface User {
+interface User 
+{
   id: string,
   name: string,
   age: number, 
   email: string,
+}
+
+interface Book
+{
+  id: string,
+  name: string,
 }
 
 console.log(addTwoNumbers);
@@ -25,7 +34,16 @@ const getUserById = (id: string): User => ({
   email: 'something@somewhere.com'
 });
 
+const getBookById = (id: string): Book => ({
+  id,
+  name: 'Typescript is better than Javascript.',
+})
 
-const user = getUserById(id: 'uuid-1o')
+const user = getUserById('uuid-1');
+const book = getBookById('book-1');
 
-const saveUser = (user: { id: string, name: string}) => console.log('saving user', {user});
+const saveUser = (user: User) => log('saving user', {user});
+const saveBook = (book: Book) => log('saving book', {book});
+
+saveUser(user)
+saveBook(book)
